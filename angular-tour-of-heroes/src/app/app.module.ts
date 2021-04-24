06f6face2e8a6edb7a80dd19e1e29093eea04b10
Pyoneru <piotrkniemczuk@gmail.com>
2021-04-24 19:31:24 +0200

    6. Routing

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -2,22 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {HeroDetailComponent} from './hero-detail.component';
 
 import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import { RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard.component';
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

7a7dcc5cf99ed87df85dc6b58f5f28b4d3b6f2f4
Pyoneru <piotrkniemczuk@gmail.com>
2021-04-26 18:39:04 +0200

    Implement TestService & Tests methods: getById, getAll, create, update, delete CRUD operations.

--- frontend-app/src/app/services/test.service.spec.ts
+++ frontend-app/src/app/services/test.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TestService } from './test.service';
+import {Hero} from "../model/hero";
 
 describe('TestService', () => {
   let service: TestService;
@@ -13,4 +14,62 @@ describe('TestService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add hero', () => {
+    // Add new hero
+    let hero: Hero = {
+      id: -1,
+      name: 'Hero'
+    }
+    service.create(hero);
+
+    expect(service.heroes.length).toBe(1);
+  })
+
+  it('new added hero should has id equal to idCounter', () =>{
+    let idCounter: number = service.idCounter;
+    // Add new hero
+    let hero: Hero = {
+      id: -1,
+      name: 'Hero'
+    }
+    service.create(hero);
+
+    expect(idCounter).toBe(hero.id);
+  })
+
+  it('updated hero should be changed', () =>{
+    let idCounter: number = service.idCounter;
+
+    // Add new hero
+    let newHero: Hero = {
+      id: -1,
+      name: 'Hero'
+    }
+    service.create(newHero);
+
+    // Change name
+    newHero.name = 'New Hero';
+    service.update(idCounter, newHero.name);
+
+    // Get hero
+    let hero: Hero;
+    service.getById(idCounter).then(data => {
+      hero = data;
+      expect(newHero.name).toEqual(data.name);
+    });
+  })
+
+  it('deleted hero should be removed from array', () =>{
+    let idCounter: number = service.idCounter;
+    // Add new hero
+    let hero: Hero = {
+      id: -1,
+      name: 'Hero'
+    }
+    service.create(hero);
+
+    service.delete(hero.id);
+    expect(service.heroes.length).toBe(0);
+  })
 });
--- frontend-app/src/app/services/test.service.ts
+++ frontend-app/src/app/services/test.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import {Hero} from "../model/hero";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
 
+  /**
+   * Heroes 'db'
+   */
+  heroes: Hero[] = [];
+
+  idCounter: number = 0;
+
   constructor() { }
+
+  /**
+   * Get all heroes
+   */
+  getAll(): Promise<Hero[]>{
+    return Promise.resolve(this.heroes);
+  }
+
+  /**
+   * Get one hero by id
+   * @param id
+   */
+  getById(id: number): Promise<Hero>{
+    return this.getAll().then( heroes => heroes.find(hero => hero.id === id));
+  }
+
+  create(hero: Hero): void{
+    hero.id = this.idCounter;
+    this.idCounter++;
+    this.heroes.push(hero);
+  }
+
+  update(id: number, heroName: string): void{
+    this.heroes.find(hero => hero.id === id).name = heroName;
+  }
+
+  delete(id: number): void{
+    let hero: Hero = this.heroes.find(h => h.id === id);
+    const index = this.heroes.indexOf(hero, 0);
+    if(index > -1){
+      delete this.heroes[index];
+      this.heroes = this.heroes.filter(hero => {return hero != null});
+    }
+  }
 }

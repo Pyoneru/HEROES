51f20f49ade021a51d4960d6bc8b3883cc697dff
Pyoneru <piotrkniemczuk@gmail.com>
2021-04-27 11:56:39 +0200

    Add Routes configuration to app (HeroRoutesModule)

---
+++ frontend-app/src/app/hero-routes/hero-routes.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {AppComponent} from "../app.component";
+
+const heroRoutes: Routes = [
+  // Default page
+  {
+    path: '',
+    redirectTo: '#'
+  },
+  // Hero List path
+  {
+    path: "#",
+    component: AppComponent // ToDo: Change Component for Hero List component
+  },
+  // New/Edit hero path
+  {
+    path: '#/hero/:id',
+    component: AppComponent // ToDo: Change Component for New/Edit Hero component.
+  },
+  // Error path
+  {
+    path: '**',
+    component: AppComponent // ToDo: Change Component for Error component.
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(heroRoutes)
+  ],
+  exports: [
+    HeroRoutesModule
+  ]
+})
+export class HeroRoutesModule { }

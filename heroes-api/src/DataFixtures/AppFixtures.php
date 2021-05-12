f3fa6102d8a77931fb645de2c11b190bf4c8fca0
Pyoneru <piotrkniemczuk@gmail.com>
2021-05-12 13:29:19 +0200

    Add test data inside AppFixture

--- heroes-api/src/DataFixtures/AppFixtures.php
+++ heroes-api/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Hero;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+
+    /**
+     * @var Faker\Factory
+     */
+    private $faker;
+
+    /**
+     * AppFixtures constructor.
+     */
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 0; $i < 15; $i++){
+            $hero = new Hero();
+            $hero->setName($this->faker->realText(20));
+            $manager->persist($hero);
+        }
 
         $manager->flush();
     }
